d79d9e9ef4bbe69dab7dd42e33c25f9eccab50de
Zander van Schoor <u23540207@tuks.co.za>
2025-05-17 20:54:46 +0200

    Initial setup for the front-end

---
+++ FireFighter/capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.apex.firefighter',
+  appName: 'FireFighter',
+  webDir: 'www'
+};
+
+export default config;
