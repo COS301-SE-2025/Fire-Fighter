cb0ed6865e4efb6d0e3fda3887d581221f7f5d5c
Zander van Schoor <u23540207@tuks.co.za>
2025-05-19 11:50:04 +0200

    Initial setup of Maven/Spring Boot for the back-end

---
+++ firefighter-platform/src/main/java/com/apex/firefighter/FireFighterAccessManagementApplication.java
@@ -0,0 +1,13 @@
+package com.apex.firefighter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FireFighterAccessManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FireFighterAccessManagementApplication.class, args);
+	}
+
+}
