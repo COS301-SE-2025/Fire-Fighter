194f0913f5be8da2bbb524550aeaf5de08c4209d
Zander van Schoor <u23540207@tuks.co.za>
2025-05-19 15:37:20 +0200

    Started with login page design

--- FireFighter/src/app/app.routes.ts
+++ FireFighter/src/app/app.routes.ts
@@ -2,12 +2,22 @@ import { Routes } from '@angular/router';
 
 export const routes: Routes = [
   {
-    path: 'home',
-    loadComponent: () => import('./home/home.page').then((m) => m.HomePage),
+    path: 'login',
+    loadComponent: () =>
+      import('./pages/login/login.page').then(m => m.LoginPage)
+  },
+  {
+    path: 'dashboard',
+    loadComponent: () =>
+      import('./pages/dashboard/dashboard.page').then(m => m.DashboardPage)
   },
   {
     path: '',
-    redirectTo: 'home',
-    pathMatch: 'full',
+    redirectTo: 'login',
+    pathMatch: 'full'
   },
+  {
+    path: '**',
+    redirectTo: 'login'
+  }
 ];
