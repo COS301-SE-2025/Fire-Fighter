557daf2f4df3105a57ab776e8fbec5a0cfaf8b94
Zander van Schoor <u23540207@tuks.co.za>
2025-05-20 20:54:19 +0200

    Revert "Added Clerk to the login page"
    This reverts commit 129cfaec5984ca6dc79e42e66739cd820f9bd440.

--- FireFighter/src/app/pages/login/login.page.ts
+++ FireFighter/src/app/pages/login/login.page.ts
@@ -1,23 +1,45 @@
-import { Component, AfterViewInit, ViewChild, ElementRef } from '@angular/core';
+// src/app/pages/login/login.page.ts
+import { Component, OnInit } from '@angular/core';
+import { CommonModule, NgClass } from '@angular/common';
+import { ReactiveFormsModule, FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { IonContent } from '@ionic/angular/standalone';
-import { CommonModule } from '@angular/common';
-
-declare const Clerk: any;
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.page.html',
   styleUrls: ['./login.page.scss'],
   standalone: true,
-  imports: [IonContent, CommonModule],
+  imports: [
+    CommonModule,
+    ReactiveFormsModule,
+    NgClass,
+    IonContent
+  ]
 })
-export class LoginPage implements AfterViewInit {
-  @ViewChild('signInContainer', { static: true }) signInContainer!: ElementRef<HTMLDivElement>;
-  loaded = false;
+export class LoginPage implements OnInit {
+  loginForm!: FormGroup;
+  isSubmitting = false;
+  errorMsg: string | null = null;
 
-  async ngAfterViewInit() {
-    await Clerk.load();
-    this.loaded = true;
-    Clerk.mountSignIn(this.signInContainer.nativeElement);
+  constructor(private fb: FormBuilder) {}
+
+  ngOnInit() {
+    // we'll use this later for validation, but right now it's just for form state
+    this.loginForm = this.fb.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.minLength(6)]]
+    });
+  }
+
+  onSubmit() {
+    // stub: we're not doing real auth yet
+    if (this.loginForm.valid) {
+      this.isSubmitting = true;
+      // Simulate API call
+      setTimeout(() => {
+        console.log('Would submit', this.loginForm.value);
+        this.isSubmitting = false;
+      }, 1500);
+    }
   }
 }
