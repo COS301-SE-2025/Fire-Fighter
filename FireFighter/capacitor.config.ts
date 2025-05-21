c970185feb7781b4151b17f20d9e16776aa3f313
Zander van Schoor <u23540207@tuks.co.za>
2025-05-21 08:46:27 +0200

    Implementation of Firebase for login and registration

--- FireFighter/capacitor.config.ts
+++ FireFighter/capacitor.config.ts
@@ -3,7 +3,14 @@ import type { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'com.apex.firefighter',
   appName: 'FireFighter',
-  webDir: 'www'
+  webDir: 'www',
+
+   plugins: {
+    FirebaseAuthentication: {
+      skipNativeAuth: false,
+      providers: ['google.com','email']
+    }
+  }
 };
 
 export default config;
--- FireFighter/src/app/app.routes.ts
+++ FireFighter/src/app/app.routes.ts
@@ -7,6 +7,10 @@ export const routes: Routes = [
       import('./pages/login/login.page').then(m => m.LoginPage)
   },
   {
+    path: 'register',
+    loadComponent: () => import('./pages/register/register.page').then( m => m.RegisterPage)
+  },
+  {
     path: 'dashboard',
     loadComponent: () =>
       import('./pages/dashboard/dashboard.page').then(m => m.DashboardPage)
