d771198890a858134f68349283dbce88008b1aa7
Sebastiaan <168179951+Sebastiaan335@users.noreply.github.com>
2025-05-30 18:23:17 +0200

    Implemented role class

--- firefighter-platform/src/main/java/com/apex/firefighter/model/Role.java
+++ firefighter-platform/src/main/java/com/apex/firefighter/model/Role.java
@@ -14,4 +14,22 @@ public class Role {
     private String name; // e.g., ADMIN, USER, FIREFIGHTER
 
     // Constructors, getters, setters
+    public Role() {
+    }
+
+    public Role(String name) {
+        this.name = name;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
