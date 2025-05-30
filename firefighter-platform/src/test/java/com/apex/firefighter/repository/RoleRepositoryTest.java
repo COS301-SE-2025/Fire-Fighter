ccd04bf09f76add4f40a705049491c1e15bc4c7b
Sebastiaan <168179951+Sebastiaan335@users.noreply.github.com>
2025-05-30 18:29:42 +0200

    Implemented Access Request

--- firefighter-platform/src/test/java/com/apex/firefighter/repository/RoleRepositoryTest.java
+++ firefighter-platform/src/test/java/com/apex/firefighter/repository/RoleRepositoryTest.java
@@ -24,5 +24,12 @@ class RoleRepositoryTest {
         Optional<Role> found = roleRepository.findByName("ADMIN");
         assertThat(found).isPresent();
         assertThat(found.get().getName()).isEqualTo("ADMIN");
+
+        Role role2 = new Role();
+        role2.setName("USER");
+        roleRepository.save(role2);
+        Optional<Role> found2 = roleRepository.findByName("USER");
+        assertThat(found2).isPresent();
+        assertThat(found2.get().getName()).isEqualTo("USER");
     }
 }
