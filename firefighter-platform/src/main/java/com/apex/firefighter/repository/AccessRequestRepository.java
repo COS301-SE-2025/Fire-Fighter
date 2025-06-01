0ebfd2e0dcb42cf776415b933817bbcf2b02f176
Zander van Schoor <u23540207@tuks.co.za>
2025-06-01 15:59:42 +0200

    Inconsistencies fixed

--- firefighter-platform/src/main/java/com/apex/firefighter/repository/AccessRequestRepository.java
+++ firefighter-platform/src/main/java/com/apex/firefighter/repository/AccessRequestRepository.java
@@ -1,10 +1,11 @@
 package com.apex.firefighter.repository;
 
 import com.apex.firefighter.model.AccessRequest;
+import com.apex.firefighter.model.AccessRequest.RequestStatus;
 import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
 
 public interface AccessRequestRepository extends JpaRepository<AccessRequest, Long> {
-    List<AccessRequest> findByStatus(String status);
+    List<AccessRequest> findByStatus(RequestStatus status);
     List<AccessRequest> findByUserId(Long userId);
 }
