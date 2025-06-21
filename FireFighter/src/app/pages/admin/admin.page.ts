265fff6c74d73408189f3cb38be2cfd0509cc0af
Franco <franco.v.d.westhuizen@gmail.com>
2025-06-21 11:03:33 +0200

    add background and navbar to admin page

--- FireFighter/src/app/pages/admin/admin.page.ts
+++ FireFighter/src/app/pages/admin/admin.page.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { IonContent } from '@ionic/angular/standalone';
+import { NavbarComponent } from '../../components/navbar/navbar.component';
 
 @Component({
   selector: 'app-admin',
   standalone: true,
-  imports: [CommonModule, IonContent],
+  imports: [CommonModule, IonContent, NavbarComponent],
   templateUrl: './admin.page.html',
   styleUrls: ['./admin.page.scss']
 })
