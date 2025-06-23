f364f23717caffb8e450390ccd7e4f91e3a835dd
MAutumn11 <markautumn11@gmail.com>
2025-06-23 15:01:47 +0200

    Delete frontend

--- FireFighter/src/environments/environment.prod.ts
+++
@@ -1,6 +0,0 @@
-export const environment = {
-  production: true,
-  apiUrl: 'http://localhost:8080/api', // Replace with your actual production API URL when ready
-  appVersion: '2.0.0-alpha.1',
-  appName: 'FireFighter'
-};
