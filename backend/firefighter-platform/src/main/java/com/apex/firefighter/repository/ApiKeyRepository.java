5d0b3a7bb7f0f189d227e1da6107cab2d61c6b56
Franco <franco.v.d.westhuizen@gmail.com>
2025-07-03 20:30:03 +0200

    add basic ApiKeyRepository class

---
+++ backend/firefighter-platform/src/main/java/com/apex/firefighter/repository/ApiKeyRepository.java
@@ -0,0 +1,14 @@
+package com.apex.firefighter.repository;
+
+import com.apex.firefighter.model.ApiKey;
+import com.apex.firefighter.model.User;
+
+
+import java.util.Optional;
+import java.util.List;
+
+@Repository
+public interface ApiKeyRepository extends JpaRepository<ApiKey, Long> {
+    Optional<ApiKey> findByApiKey(String apiKey);
+    List<ApiKey> findByUser(User user);
+}
