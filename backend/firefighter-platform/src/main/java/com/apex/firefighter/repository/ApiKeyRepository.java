2ea2291d0a6b774b5dc7b32dde44868bc9be7229
Franco <franco.v.d.westhuizen@gmail.com>
2025-07-03 20:30:44 +0200

    import additional required libraries to the ApiKeyRepository class

--- backend/firefighter-platform/src/main/java/com/apex/firefighter/repository/ApiKeyRepository.java
+++ backend/firefighter-platform/src/main/java/com/apex/firefighter/repository/ApiKeyRepository.java
@@ -3,6 +3,8 @@ package com.apex.firefighter.repository;
 import com.apex.firefighter.model.ApiKey;
 import com.apex.firefighter.model.User;
 
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 import java.util.List;
