862d0a831b55456a43db90b27ee50957ffae706e
Franco <franco.v.d.westhuizen@gmail.com>
2025-07-03 18:41:42 +0200

    add implementation for api key generation

---
+++ backend/firefighter-platform/src/main/java/com/apex/firefighter/service/ApiKeyGenerator.java
@@ -0,0 +1,10 @@
+package com.apex.firefighter.service;
+
+import java.security.SecureRandom;
+import java.util.Base64;
+
+public class ApiKeyGenerator {
+    private static final int API_KEY_BYTE_LENGTH = 32;
+
+    
+} 
