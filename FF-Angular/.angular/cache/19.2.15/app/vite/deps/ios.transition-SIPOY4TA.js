72e103610ff7411e8478e8e9b84469a7402e18c7
Franco <franco.v.d.westhuizen@gmail.com>
2025-07-23 14:04:17 +0200

    update file structure

---
+++ FF-Angular/.angular/cache/19.2.15/app/vite/deps/ios.transition-SIPOY4TA.js
@@ -0,0 +1,14 @@
+import {
+  iosTransitionAnimation,
+  shadow
+} from "./chunk-C3TCO22T.js";
+import "./chunk-HPCN2MB7.js";
+import "./chunk-CKFPX5IG.js";
+import "./chunk-R4SJUBFW.js";
+import "./chunk-44IZU6OF.js";
+import "./chunk-CBIR4FRL.js";
+import "./chunk-UQIXM5CJ.js";
+export {
+  iosTransitionAnimation,
+  shadow
+};
