414630dfe17c0b13f0f441f5a1302c3982f7915b
Franco <franco.v.d.westhuizen@gmail.com>
2025-08-07 15:58:18 +0200

    fixed token expiration issue, firebase integration issue, add missing imports in test configuration file, resolve merge conflicts and expand on security test coverage

--- FF-API/firefighter-platform/src/test/java/com/apex/firefighter/FireFighterAccessManagementApplicationTests.java
+++ FF-API/firefighter-platform/src/test/java/com/apex/firefighter/FireFighterAccessManagementApplicationTests.java
@@ -1,11 +1,14 @@
 package com.apex.firefighter;
 
+import com.apex.firefighter.config.TestConfig;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Import;
 import org.springframework.test.context.ActiveProfiles;
 
 @ActiveProfiles("test")
 @SpringBootTest
+@Import(TestConfig.class)
 class FireFighterAccessManagementApplicationTests {
 
 	@Test
