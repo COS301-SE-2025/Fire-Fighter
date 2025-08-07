1b916d7b8363d876473410555bc6c1bcccec23ae
Zander van Schoor <u23540207@tuks.co.za>
2025-08-07 21:23:48 +0200

    Added consistent header

--- FF-Angular/src/app/pages/metrics/metrics.page.ts
+++ FF-Angular/src/app/pages/metrics/metrics.page.ts
@@ -1,20 +1,28 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { IonContent, IonHeader, IonTitle, IonToolbar } from '@ionic/angular/standalone';
+import { IonContent, IonRefresher, IonRefresherContent } from '@ionic/angular/standalone';
+import { NavbarComponent } from '../../components/navbar/navbar.component';
 
 @Component({
   selector: 'app-metrics',
   templateUrl: './metrics.page.html',
   styleUrls: ['./metrics.page.scss'],
   standalone: true,
-  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule]
+  imports: [IonContent, IonRefresher, IonRefresherContent, CommonModule, FormsModule, NavbarComponent]
 })
 export class MetricsPage implements OnInit {
 
   constructor() { }
 
   ngOnInit() {
+  }
+
+  doRefresh(event: any) {
+    // Refresh metrics data here
+    setTimeout(() => {
+      event.target.complete();
+    }, 1000);
   }
 
 }
