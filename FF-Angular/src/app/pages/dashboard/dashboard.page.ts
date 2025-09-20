5c07daa282d618844acb22e814b251f976423e0e
Franco <franco.v.d.westhuizen@gmail.com>
2025-09-20 19:11:49 +0200

    remove token component from dashboard page

--- FF-Angular/src/app/pages/dashboard/dashboard.page.ts
+++ FF-Angular/src/app/pages/dashboard/dashboard.page.ts
@@ -7,7 +7,6 @@ import { TicketService, Ticket } from '../../services/ticket.service';
 import { AdminService, AdminTicket } from '../../services/admin.service';
 import { NotificationService } from '../../services/notification.service';
 import { NavbarComponent } from '../../components/navbar/navbar.component';
-import { TokenStatusComponent } from '../../components/token-status.component';
 import { calculateTimeAgo } from '../../services/mock-ticket-database';
 import { catchError, finalize } from 'rxjs/operators';
 import { of, Subscription } from 'rxjs';
@@ -35,7 +34,6 @@ export interface Activity {
     IonRefresher,
     IonRefresherContent,
     NavbarComponent,
-    TokenStatusComponent,
     TranslateModule
   ]
 })
