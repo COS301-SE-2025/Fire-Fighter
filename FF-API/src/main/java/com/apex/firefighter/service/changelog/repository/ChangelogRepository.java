ed007133eda2442037705ddefd448bf3e536f77e
MAutumn11 <markautumn11@gmail.com>
2025-09-21 09:37:02 +0200

    Revert "Create ChangelogRepository.java"
    This reverts commit d0cd4b104b3e8a10f3fb57b4489fc9ec4a0da25a.

--- FF-API/src/main/java/com/apex/firefighter/service/changelog/repository/ChangelogRepository.java
+++
@@ -1,9 +0,0 @@
-package com.apex.firefighter.service.changelog.repository;
-
-import com.apex.firefighter.service.changelog.model.Changelog;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ChangelogRepository extends JpaRepository<Changelog, Long> {
-}
